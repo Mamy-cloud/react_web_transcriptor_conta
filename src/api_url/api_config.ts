72ae7e093b2412de7f3cdc537a81602f076aa097
Mamy-cloud <mamycompteprofessionnel@gmail.com>
2026-04-24 17:31:14 +0300

    changement du nom de domaine pour render

--- src/api_url/api_config.ts
+++ src/api_url/api_config.ts
@@ -5,7 +5,7 @@
    la variable BASE_URL ici.
    ================================================ */
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'http://localhost:8000'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'https://conta-backend-fastapi.onrender.com'
 
 export const API = {
   BASE_URL,
